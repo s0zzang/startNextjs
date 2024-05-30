59a053caa3d3bcb9ba389eec7e0dfccd33576429
s0zzang <dlth8888@gmail.com>
2024-05-30 17:04:59 +0900

    feat. data fetching, loading

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,17 @@
-"use client";
-import { useEffect, useState } from "react";
-
+export const metadata = {
+  title: "Home",
+};
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const getMovies = async () => {
-    const res = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await res.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovie() {
+  await new Promise((res) => setTimeout(res, 3000));
 
-  return <div>{isLoading ? "로딩중 ..." : JSON.stringify(movies)}</div>;
+  const res = await fetch(URL);
+  const json = await res.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovie();
+  return <div>{JSON.stringify(movies)}</div>;
 }
