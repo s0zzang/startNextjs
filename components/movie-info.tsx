e8cc29d9ef8492d91f3f2f18bccab5cd0e17f8ce
s0zzang <dlth8888@gmail.com>
2024-06-01 20:07:25 +0900

    design. movie detail

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 const getMovie = async (id: string) => {
   await new Promise((res) => setTimeout(res, 2000));
@@ -8,7 +9,36 @@ const getMovie = async (id: string) => {
 
 const MovieInfo = async ({ id }: { id: string }) => {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  console.log(movie.genres);
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={styles.poster}
+      />
+      <div className={styles.info}>
+        <h2 className={styles.title}>{movie.title}</h2>
+        <h3>★ {movie.vote_average.toFixed(1)}</h3>
+        <ul>
+          {movie["production_companies"].map((item) => {
+            <li>
+              <img src={item.logo_path} alt={item.name} />
+            </li>;
+          })}
+        </ul>
+        <ul>
+          {movie["genres"].map((item) => {
+            <li>{item.name}</li>;
+          })}
+        </ul>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default MovieInfo;
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,8 +1,7 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 const getVideos = async (id: string) => {
-  // await new Promise((res) => setTimeout(res, 5000));
-  // throw new Error("something broke...");
   const res = await fetch(`${API_URL}/${id}/videos`);
   return res.json();
 };
@@ -10,7 +9,19 @@ const getVideos = async (id: string) => {
 const MovieVideos = async ({ id }: { id: string }) => {
   const videos = await getVideos(id);
 
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MovieVideos;
